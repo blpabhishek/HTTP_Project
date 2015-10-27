42dca3dc9664df223cf9fd606a0ed7b060725d13
Abhishek Gupta <abhishkg@thoughtworks.com>
2015-10-27 21:02:43 +0530

    Added Get Method and Writen code to write file

--- Client.js
+++ Client.js
@@ -1,36 +1,54 @@
 var http=require('http');
-var os=require('os');
 var querystring=require('querystring');
-var username=os.hostname();
+var username=require('os').hostname();
 
 var onError=function(e) {
 	console.error("Some thing Went Wrong:",e.message);
 };
 
 var postMethod=function(host,actualdata){
-var message={};
-message[username]=actualdata;
-var postData = querystring.stringify(message);
-var options = {
-  hostname: host,
-  port: 8000,
-  method: 'POST',
-  headers: {
-    'Content-Type': 'text',
-    'Content-Length': postData.length
-  }
-};
+  var message={};
+  message[username]=actualdata;
+  message['destination']='localhost';
+  var postData = querystring.stringify(message);
+  var options = {
+    hostname: host,
+    port: 8002,
+    method: 'POST',
+    headers: {'Content-Type': 'text','Content-Length': postData.length}
+  };
 
-var responce=function(res) {		
-	res.on('data', function (chunk) {
-    console.log(chunk.toString());
-  });
-}
+  var responce=function(res) {		
+  	res.on('data', function (chunk) {
+      console.log(chunk.toString());
+    });
+  }
 var req = http.request(options,responce).on('error',onError);
 req.write(postData);
 req.end();
 };
+
+
+var getMethod=function(host){
+var options={host:host,port:8002,method:'GET',path:'/getMessage/'+username};
+  var onResponce=function(res) {
+    var data='';
+    res.setEncoding('utf8');
+      res.on('data',function(chunk){
+        data+=chunk;
+    });
+      res.on('end',function(){
+        console.log(querystring.parse(data.toString()));
+      })
+  };
+http.request(options,onResponce).on('error',onError).end();
+}
+
+
 var text=process.argv.slice(2).join(' ').trim();
 if(!text)
-  text="hello this is testing";//Default Message
-postMethod('localhost',text); 
+  text="hello this is testing";//Default message
+postMethod('localhost',text); //Pnadey's IP 10.4.23.176
+getMethod('localhost');
+//setTimeout(getMethod('localhost'),10000);
+//setTimeout(getMethod('localhost'),10000);
--- Server.js
+++ Server.js
@@ -1,15 +1,34 @@
 var http = require('http');
-var os=require('os');
-var username=os.hostname();
+var conversations={};
 var querystring=require('querystring');
+var fs=require('fs');
 var server=function(request,response){
+	var data='';
 	response.writeHead(200,{'Content-Type':'text/plain'});
 	request.setEncoding('utf8');
-	console.log("request:",request.headers);
 	request.on('data',function(chunk){
-		console.log(querystring.parse(chunk.toString()));
+		data+=chunk;
 	});
-   	response.end("Connected to "+username+"'s Server");
+	request.on('end',function(){
+		if(request.method.toLowerCase()=='post'){
+		var message=querystring.parse(data.toString());
+		var user=Object.keys(message)[0];
+		fs.appendFileSync('./'+user+'data.txt',JSON.stringify(message));	
+		}
+	});
+	if(request.method.toLowerCase()=='get'){
+		var user=getUserName(request);
+		var data=fs.readFileSync('./'+user+'data.txt','utf8');
+		fs.unlinkSync('./'+user+'data.txt');
+		response.write(data);
+	}
+   	response.end();
 };
-http.createServer(server).listen(8000);
+http.createServer(server).listen(8002);
+
+var getUserName=function(request){
+	if(request.method.toLowerCase()=='get'){
+		return request.url.split('/')[2];
+	}
+}
   	
